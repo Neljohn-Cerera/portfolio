49a18175709db4486362eee431335caa0805c507
Nel John R. Cerera <neljohncerera.github@gmail.com>
2022-09-28 23:45:33 +0800

    setup text and images

---
+++ src/assets/images/index.ts
@@ -0,0 +1,5 @@
+import img_mongodb from "./skills_mongodb.png";
+import img_mobile from "mobile.jpg";
+import img_certficate from "certificate.png";
+
+export { img_mongodb, img_mobile, img_certficate };
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,33 @@ module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./src/components/**/*.{js,ts,jsx,tsx}",
+    "./src/layout/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    colors: {
+      primary: "hsl(var(--color-primary-green-dark) / <alpha-value>)",
+      secondary: "hsl(var(--color-primary-green-light) / <alpha-value>)",
+      "yellow-main": "hsl(var(--color-accent-yellow) / <alpha-value>)",
+      white: "hsl(var(--color-neutral-white) / <alpha-value>)",
+      red: "red",
+    },
+    container: {
+      padding: {
+        DEFAULT: "1rem",
+        sm: "2rem",
+        lg: "4rem",
+        xl: "5rem",
+        "2xl": "6rem",
+      },
+    },
+    fontFamily: {
+      poppins: ["Poppins", "sans-serif"],
+    },
+    fontWeight: {
+      regular: 400,
+      semibold: 600,
+      bold: 700,
+    },
     extend: {},
   },
   plugins: [],
