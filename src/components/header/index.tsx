b1924a25907198d249663b08f28db039f66f8e56
Nel John R. Cerera <neljohncerera.github@gmail.com>
2022-10-04 22:25:20 +0800

    added header mobile navigation

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,21 +1,34 @@
 import { DownloadIcon, MenuIcon, MyLogo } from "@assets/icons";
+import classnames from "classnames";
 import React from "react";
+import { useState } from "react";
 
 const Header = () => {
+  const [openMenu, setOpenMenu] = useState<boolean | undefined>(undefined);
+
+  const handleClickOpenMenu = () => {
+    !openMenu ? setOpenMenu(true) : openMenu ? setOpenMenu(false) : undefined;
+  };
   return (
     <header className="header">
       {/* header logo */}
       <MyLogo classname="header-logo" fill="#ffffff" />
 
       {/* header button menu */}
-      <button className="btn btn-menu">
+      <button className="btn btn-menu" onClick={handleClickOpenMenu}>
         <MenuIcon classname="btn-menu__icon" />
       </button>
 
       {/* header navigation */}
       <nav className="header-navigation">
         {/* header navigation list */}
-        <ul role={"list"} className="header-navigation__list">
+        <ul
+          role={"list"}
+          className={classnames("header-navigation__list", {
+            "header-navigation__list--animate": openMenu == true,
+            "header-navigation__list--animate-close": openMenu == false,
+          })}
+        >
           <li className="header-navigation__item">
             <a href="Home">Home</a>
           </li>
@@ -31,8 +44,12 @@ const Header = () => {
           <li className="header-navigation__item">
             <a href="#contactme">Contact Me</a>
           </li>
+          <li className="header-navigation__item-download">
+            <a href="#contactme">Download CV</a>
+          </li>
         </ul>
       </nav>
+
       {/* header button download cv */}
       <button className="btn btn-download">
         Download CV <DownloadIcon classname="btn-download__icon" />
