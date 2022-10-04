872c4ba9d189ea8282f94d7d4380856a7d252d04
Nel John R. Cerera <neljohncerera.github@gmail.com>
2022-10-04 22:54:19 +0800

    fix overflow horizontal mobile screens

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,20 @@
 import type { AppProps } from "next/app";
+import Head from "next/head";
 // import Layout from "../src/layout";
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        {/* if not set overflow x will show in the page */}
+        {/* note: this must be set in app.js ref: https://nextjs.org/docs/messages/no-document-viewport-meta */}
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </Head>
+      <Component {...pageProps} />
+      );
+    </>
+  );
 }
 
 export default MyApp;
