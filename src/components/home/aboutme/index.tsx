4734f87d9eb5ff3366a6d629b5978ee2acb096a1
Nel John R. Cerera <neljohncerera.github@gmail.com>
2022-10-05 16:25:04 +0800

    add aboutme and whatido animation

--- src/components/home/aboutme/index.tsx
+++ src/components/home/aboutme/index.tsx
@@ -1,21 +1,45 @@
 import { img_me } from "@assets/images";
 import { Image, Title } from "@components/shared";
+import { useInView } from "react-intersection-observer";
+import classnames from "classnames";
 
 const AboutMeSection = () => {
+  const { ref, inView } = useInView({
+    threshold: 0,
+    triggerOnce: true,
+  });
+
+  console.log("about me inView : ", inView);
+
   return (
-    <section id="aboutme" className="aboutme-section">
+    <section id="aboutme" className={classnames("aboutme-section")}>
       <div className="container">
-        <Title.Section classname="aboutme-title">About Me</Title.Section>
+        <Title.Section
+          classname={classnames("aboutme-title", {
+            "aboutme-title--animate": inView,
+          })}
+        >
+          About Me
+        </Title.Section>
 
         {/* about me details */}
-        <p className="aboutme-details">
+        <p
+          ref={ref}
+          className={classnames("aboutme-details", {
+            "aboutme-details--animate": inView,
+          })}
+        >
           {"I'm"} a recent college graduate with a degree of Bachelor of Science
           in Information Technology who is passionate and committed to website
           developing and integrating Android applications.
         </p>
 
         {/* about me img container */}
-        <div className="aboutme-img__container">
+        <div
+          className={classnames("aboutme-img__container", {
+            "aboutme-img__container--animate": inView,
+          })}
+        >
           <Image
             src={img_me}
             alt="Graduation picture"
--- src/components/home/whatido/index.tsx
+++ src/components/home/whatido/index.tsx
@@ -1,36 +1,29 @@
-import {
-  DataStructureIcon,
-  ResponsiveMobileIcon,
-  MobileIcon,
-  CodeIcon,
-} from "@assets/icons";
 import { Title } from "@components/shared";
+import { useInView } from "react-intersection-observer";
+import classnames from "classnames";
+import ListWhatIdo from "./list";
 
 const WhatIdoSection = () => {
+  const { ref, inView } = useInView({
+    threshold: 0,
+    triggerOnce: true,
+  });
+
+  console.log("what i do inView : ", inView);
+
   return (
     <div id="whatido" className="whatido-section">
       <div className="container">
-        <Title.Section classname="whatido-title">WHAT I DO</Title.Section>
+        <Title.Section
+          classname={classnames("whatido-title", {
+            "whatido-title--animate": inView,
+          })}
+        >
+          WHAT I DO
+        </Title.Section>
+
         {/* whatido list */}
-        <ul role="list" className="whatido__list">
-          <li className="whatido__item">
-            <ResponsiveMobileIcon classname="whatido__item-icon" />
-            <p className="whatido__item-detail">Responsive Design</p>
-          </li>
-          <li className="whatido__item">
-            <MobileIcon classname="whatido__item-icon" />
-            <p className="whatido__item-detail">Mobile First</p>
-          </li>
-          <li className="whatido__item">
-            <CodeIcon classname="whatido__item-icon" />
-            <p className="whatido__item-detail">Design to Code</p>
-          </li>
-          <li className="whatido__item">
-            <DataStructureIcon classname="whatido__item-icon" />
-            <p className="whatido__item-detail">Data Structure</p>
-          </li>
-        </ul>
-        {/* <div className="whatido__line"></div> */}
+        <ListWhatIdo ref={ref} inView={inView!} />
       </div>
     </div>
   );
